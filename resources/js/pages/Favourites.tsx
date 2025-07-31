748b6116b328428dba169c4a31508d6ef78c8019
Daf <8thatfrog@gmail.com>
2025-07-31 23:21:25 +0100

    Favourites mobile

--- resources/js/pages/Favourites.tsx
+++ resources/js/pages/Favourites.tsx
@@ -18,16 +18,15 @@ export default function Favourites({ items } : any) {
                     <div key={`${item.address}-${item.nft_id}`} 
                             className="mb-4 p-4 border rounded hover:bg-muted/50 cursor-pointer"
                             onClick={() => router.visit(`/items/${item.address}/${item.nft_id}`)}>
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center w-full">
                             <div className="flex">
                                 <img className="w-12 h-12 rounded-full" src={item.image_url}></img>
                                 <div className="pl-3">
                                     <h3 className="text-lg font-semibold">{item.name || "Unknown"}</h3>
-                                    <div className="flex">
-                                        <p className="text-gray-600">{item.address}</p>
-                                        <p className="text-white/80">&nbsp; / &nbsp;</p>
-                                        <p className="text-gray-600">{item.nft_id}</p>
+                                    <div className="flex break-all">
+                                        <span className="text-gray-500">{item.address}</span>
                                     </div>
+                                    <span className="text-gray-600"> #{item.nft_id}</span>
                                 </div>
                             </div>
                         </div>
